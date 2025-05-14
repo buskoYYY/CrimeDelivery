69177eee7b079371f6b5b8d937a1d923aa7fcf89
buskoYYY <busko.y.y.y@gmail.com>
2025-05-14 18:03:17 +0300

    Added car spawner

---
+++ Delivery/Assets/ArcadeIdleEngine/Scripts/CarSpawner.cs
@@ -0,0 +1,15 @@
+using ArcadeBridge.ArcadeIdleEngine.Interactables;
+using UnityEngine;
+
+namespace ArcadeBridge
+{
+    public class CarSpawner : MonoBehaviour
+    {
+        [SerializeField] private Car _car;
+        public void CreateCar()
+        {
+            Instantiate(_car
+                );
+        }
+    }
+}
